e2c3e5c0833659ad0867895f095b6a1cca6f0caf
debo9210 <omololuadebowale1@gmail.com>
2019-03-24 16:58:34 +0200

    Style home  page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
   def home
 
   end
+
+  def gallery
+
+  end
+
+  def about
+
+  end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    root 'pages#home'
+   get 'gallery', to: 'pages#gallery'
+   get 'about', to: 'pages#about'
+   get 'contact', to: 'pages#contact'
 end
